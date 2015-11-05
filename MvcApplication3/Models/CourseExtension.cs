0d4b0c5f9159704b569fe28071af03847aa48a9e
Gerald Fischer <gerald.k.fischer@gmail.com>
2015-11-05 20:53:19 +0100

    added UI date support in course

---
+++ MvcApplication3/Models/CourseExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication3.Models
+{
+    [MetadataType(typeof(CourseMetaData))]
+    public partial class Course
+    {
+    }
+    public partial class CourseMetaData
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> Date { get; set; }
+    }
+}
