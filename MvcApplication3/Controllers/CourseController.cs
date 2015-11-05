04e9fc7836caf141e2a1fa4da735b38a10e88874
Gerald fischer <g.fischer@newmagic.at>
2015-11-05 20:01:03 +0100

    initial

---
+++ MvcApplication3/Controllers/CourseController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication3.Models;
+
+namespace MvcApplication3.Controllers
+{
+    public class CourseController : Controller
+    {
+        private LikEntities db = new LikEntities();
+
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View(db.Courses.ToList());
+        }
+
+        //
+        // GET: /Default1/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        //
+        // GET: /Default1/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Courses.Add(course);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(course);
+        }
+
+        //
+        // GET: /Default1/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        //
+        // POST: /Default1/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(course).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(course);
+        }
+
+        //
+        // GET: /Default1/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        //
+        // POST: /Default1/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Course course = db.Courses.Find(id);
+            db.Courses.Remove(course);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
